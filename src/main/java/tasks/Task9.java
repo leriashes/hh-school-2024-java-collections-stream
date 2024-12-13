d3e25dfecc58f1bedb94fedb28e4440ef89e7bea
leriashes <valeriya-shevchenko-2002@mail.ru>
2024-12-13 15:41:18 +0700

    Complete Task 9

--- src/main/java/tasks/Task9.java
+++ src/main/java/tasks/Task9.java
@@ -1,14 +1,8 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -21,73 +15,88 @@ P.P.S Здесь ваши правки необходимо прокоммент
  */
 public class Task9 {
 
-  private long count;
+  // Неиспользуемое поле count удалено
 
   // Костыль, эластик всегда выдает в топе "фальшивую персону".
   // Конвертируем начиная со второй
+
+  /*
+  Минусы исходного решения
+  - метод remove изменяет исходный список людей
+  - в случае с неизменяемым списком в remove - будет ошибка
+   */
   public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::firstName).collect(Collectors.toList());
+    return persons.stream()
+        .skip(1)
+        .map(Person::firstName)
+        .collect(Collectors.toList());
   }
 
   // Зачем-то нужны различные имена этих же персон (без учета фальшивой разумеется)
+  // Нет необходимости использовать стрим и distinct, поскольку можно сразу создать множество
   public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+    return new HashSet<>(getNames(persons));
   }
 
   // Тут фронтовая логика, делаем за них работу - склеиваем ФИО
+  /*
+  - добавлено отчество (в исходном решении повторно добавлялась фамилия вместо отчества)
+  - использован стрим для проверки на null и склейки через разделители, более компактно
+   */
   public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.secondName() != null) {
-      result += person.secondName();
-    }
-
-    if (person.firstName() != null) {
-      result += " " + person.firstName();
-    }
-
-    if (person.secondName() != null) {
-      result += " " + person.secondName();
-    }
-    return result;
+    return Stream.of(person.secondName(), person.firstName(), person.middleName())
+        .filter(Objects::nonNull)
+        .collect(Collectors.joining(" "));
   }
 
   // словарь id персоны -> ее имя
+  // Можно сразу собирать в словарь при помощи стрима, более компактно, без доп. переменных и проверок "вручную"
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.id())) {
-        map.put(person.id(), convertPersonToString(person));
-      }
-    }
-    return map;
+    return persons.stream()
+        .collect(Collectors.toMap(Person::id, this::convertPersonToString, (first, second) -> first));
   }
 
   // есть ли совпадающие в двух коллекциях персоны?
+  /*
+  Минусы исходного решения:
+  - сложность O(n * m)
+  - даже когда человек из persons1 нашёлся в persons2 продолжается его поиск (сравнение с остальными)
+
+  Можно использовать стрим и хэш-сет, более быстро и лаконично
+  + anyMatch для person остановится как только найдёт совпадающего в persons2
+  + поиск в хэш-сет O(1)
+  + итоговая сложность O(n + m)
+  - использование памяти
+   */
+  //
+  // В исходном решении сложность O(n * m)
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
-    }
-    return has;
+    return persons1.stream().anyMatch(new HashSet<>(persons2)::contains);
   }
 
   // Посчитать число четных чисел
+  /*
+    Минусы использования переменной count:
+    - использование памяти
+    - если в дальнейшем count будет использоваться в другом методе, как свойство объекта, а не счётчик,
+    то вызов метода countEven может непредсказуемо изменять данную переменную
+    - при использовании метода (или просто изменении count) сразу несколькими потоками значение count непредсказуемо (проблема гонки данных)
+  */
   public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+    return numbers.filter(num -> num % 2 == 0).count();
   }
 
   // Загадка - объясните почему assert тут всегда верен
   // Пояснение в чем соль - мы перетасовали числа, обернули в HashSet, а toString() у него вернул их в сортированном порядке
+  /*
+  В данном случае assert будет всегда true потому, что количество buckets (корзин) в хэш-таблице для HashSet
+  будет расчитано относительно числа элементов (так, чтобы число элементов не превышало 0.75 от числа бакетов),
+  а hashCode для int равен самому числу,
+  поэтому получится, что каждое число будет "лежать" в своей корзине
+
+  При получении элементов множества, они будут выданы в порядке номеров корзин,
+  следовательно, в порядке возрастания
+   */
   void listVsSet() {
     List<Integer> integers = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
     List<Integer> snapshot = new ArrayList<>(integers);
